ee42f05a596678e1617a68b2842d53437f29a7ba
quan26dec <49353022+quan26dec@users.noreply.github.com>
2026-08-20 10:34:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -519,7 +519,7 @@ early_break55_score_df = early_break55_score_df.merge(name_map_df, on="Code", ho
 early_break55_score_df = early_break55_score_df.rename(columns={"CoName": "銘柄名"})
 early_break55_score_df = early_break55_score_df.reset_index(drop=True)
 early_break55_score_df.index = early_break55_score_df.index + 1
-early_break55_score_df = early_break55_score_df[["Code", "銘柄名", "C", "High55", "Break55Pct", "Vo", "Vol20", "VolRatio", "TurtleScore"]]
+early_break55_score_df = early_break55_score_df[["Code", "銘柄名", "C", "High55", "Break55Pct", "Vo", "Vol20", "AvgTradingValue20", "VolRatio", "TurtleScore"]]
 early_break55_score_df = early_break55_score_df.rename(columns={
     "Code": "銘柄コード",
     "C": "終値",
@@ -527,6 +527,7 @@ early_break55_score_df = early_break55_score_df.rename(columns={
     "Break55Pct": "55日高値上抜け率(%)",
     "Vo": "最新出来高",
     "Vol20": "20日平均出来高",
+    "AvgTradingValue20": "20日平均売買代金",
     "VolRatio": "出来高倍率",
 })
 
